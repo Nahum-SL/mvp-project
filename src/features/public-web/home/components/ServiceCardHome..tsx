1bf10ff3a7c7413f7cd80d61d656e9230ef7f656
Nahum SL <nahum2006sl@gmail.com>
2026-04-07 01:11:38 -0500

    fix

--- src/features/public-web/home/components/ServiceCardHome..tsx
+++ src/features/public-web/home/components/ServiceCardHome..tsx
@@ -32,6 +32,7 @@ export const ServiceCardHome = ({
         <Image
           src={`/${service.image}.webp`}
           alt={service.title}
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 600px"
           fill
           className="object-cover group-hover:scale-105 transition-transform duration-500"
         />
